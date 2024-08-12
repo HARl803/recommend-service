1c123e1c559e651ba5cc1031b57be7ba3340a4b9
junhokim <papaci222@gmail.com>
2024-08-12 17:04:25 +0900

    � docs[#2]: view 작성 및 urls.py에 preview_data 추가 및 테스트

--- estimate/urls.py
+++ estimate/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'estimate'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('preview_data/', views.preview_data, name='preview_data'),
 ]
 
--- estimate/views.py
+++ estimate/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from . import train_model
 
-# Create your views here.
 def index(request):
     pass
+
+# 전처리 데이터 확인
+def preview_data(request):
+    df, encoder = train_model.process_data()
+    sample_data = df.head(10).to_dict(orient='records')
+    return JsonResponse(sample_data, safe=False)
